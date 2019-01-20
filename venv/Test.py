44b7a900270a23896a16e3934b1b00dd39a157c9
FriendF <indiana228@gmail.com>
2019-01-20 13:53:19 +0500

    Добавил фреймвор unittest. Переоформил код теста на этот фреймворк

--- venv/Test.py
+++ venv/Test.py
@@ -1,23 +1,34 @@
 from selenium import webdriver
 import os
-import test_settings
-chrome_path = r"D:\My Files\Project\MyFirstProject\chromedriver_win32\chromedriver.exe"
-driver = webdriver.Chrome(chrome_path) # Инициализация веб-драйвера
-driver.maximize_window() #выставление максимального размера окна
-driver.implicitly_wait(15)
-driver.get("https://www.epicgames.com/") #переход на сайт
-newpath = r"D:\\Test\\"+test_settings.test_day()+' TEST'
-os.makedirs(newpath)
-driver.get_screenshot_as_file(newpath+'\\'+test_settings.test_time()+'.png')
-driver.find_element_by_xpath("//div[@id='user']").click()
-driver.find_element_by_xpath("//a[@id='switchPageAction']").click()
-driver.find_element_by_xpath("//input[@id='name']").send_keys("Тест")
-driver.find_element_by_xpath("//input[@id='lastName']").send_keys("Тестовый")
-driver.find_element_by_xpath("//input[@id='displayName']").send_keys("Selenium_test")
-driver.find_element_by_xpath("//input[@id='email']").send_keys("test mail")
-driver.find_element_by_xpath("//input[@id='password']").send_keys("testPassword1")
-driver.find_element_by_xpath("//div[@class='checkContainer termsAgree filled']").click()
-driver.find_element_by_xpath("//div[@class='btnContainer filled']").click()
-assert "Введите действующий адрес электронной почты." in driver.page_source
-driver.get_screenshot_as_file(newpath+'\\'+test_settings.test_time()+'.png')
-driver.close()
+from test_settings import TestSettings
+import unittest
+
+class TestReg (unittest.TestCase):
+    def setUp(self):
+        chrome_path = r"D:\My Files\Project\MyFirstProject\chromedriver_win32\chromedriver.exe"
+        self.driver = webdriver.Chrome(chrome_path)  # Инициализация веб-драйвера
+        self.driver.maximize_window()  # выставление максимального размера окна
+        self.driver.implicitly_wait(15)
+    def test_WrongMail(self):
+        newpath = TestSettings.CreatePath() + "WrongMail"
+        if not os.path.exists(newpath):
+            os.makedirs(newpath)
+        self.driver.get("https://www.epicgames.com/")  # переход на сайт
+        self.driver.get_screenshot_as_file(TestSettings.TakeScreenshot(newpath))
+        self.driver.find_element_by_xpath("//div[@id='user']").click()
+        self.driver.find_element_by_xpath("//a[@id='switchPageAction']").click()
+        self.driver.find_element_by_xpath("//input[@id='name']").send_keys("Тест")
+        self.driver.find_element_by_xpath("//input[@id='lastName']").send_keys("Тестовый")
+        self.driver.find_element_by_xpath("//input[@id='displayName']").send_keys("Selenium_test")
+        self.driver.find_element_by_xpath("//input[@id='email']").send_keys("test mail")
+        self.driver.find_element_by_xpath("//input[@id='password']").send_keys("testPassword1")
+        self.driver.find_element_by_xpath("//div[@class='checkContainer termsAgree filled']").click()
+        self.driver.find_element_by_xpath("//div[@class='btnContainer filled']").click()
+        assert "Введите действующий адрес электронной почты." in self.driver.page_source
+        self.driver.get_screenshot_as_file(TestSettings.TakeScreenshot(newpath))
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == '__main__':
+    unittest.main()
--- venv/test_settings.py
+++ venv/test_settings.py
@@ -1,7 +1,23 @@
 import datetime
 
-def test_day ():
-    return datetime.datetime.now().strftime("%d-%m-%Y")
+class TestSettings:
+    def test_day():
+        return datetime.datetime.now().strftime("%d-%m-%Y")
 
-def test_time ():
-    return datetime.datetime.now().strftime("%H-%M-%S")
+    def test_time():
+        return datetime.datetime.now().strftime("%H-%M-%S")
+
+    def CreatePath():
+        path = r"D:\\Test\\"+TestSettings.test_day()+" - "
+        return path
+
+    def TakeScreenshot(path):
+        screenpath = path+'\\' + TestSettings.test_time() + ".png"
+        return screenpath
+
+
+
+
+
+
+
